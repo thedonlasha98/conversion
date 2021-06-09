633098636d8a2f1ef29278cbd0c38ee38f4cf7cd
thedonlasha98 <thedonlasha@gmail.com>
2021-06-10 02:27:37 +0400

    * Clean Code

--- src/main/java/ge/bog/conversion/ropository/ConversionRepository.java
+++ src/main/java/ge/bog/conversion/ropository/ConversionRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConversionRepository extends CrudRepository<Conversion, Long> {
-}
+public interface ConversionRepository extends CrudRepository<Conversion, Long> {}
